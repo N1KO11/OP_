2a7fa9c40a4250000f91175cb0d1b97274938362
Никита <112944783+N1KO11@users.noreply.github.com>
2022-12-21 22:29:18 +0300

    Create №11.h

---
+++ 11/№11.h
@@ -0,0 +1,9 @@
+#ifndef asd
+#define asd
+struct coordinate {
+    float x;
+    float y;
+};
+float P(struct coordinate, struct coordinate, struct coordinate);
+float S(struct coordinate, struct coordinate, struct coordinate);
+#endif
