70393c2b0432e88fba3b5958c5ce59c0309ba715
Никита <112944783+N1KO11@users.noreply.github.com>
2022-12-21 22:24:36 +0300

    Create №3.c

---
+++ 7/№3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+union {
+    struct {
+        int play : 1;
+        int pause : 1;
+        int record : 1;
+    };
+    char tags;
+}sost;
+int main() {
+    scanf("%x", &sost.tags);
+    if (sost.play)
+        printf("Music is playing\n");
+    else
+        printf("Music isnt playing\n");
+    if (sost.pause & !sost.play)
+        printf("Music is on pause\n");
+    else
+        printf("Music isnt on pause\n");
+    if (sost.record)
+        printf("Recording...\n");
+    else
+        printf("Not recording\n");
+    return 0;
+}
