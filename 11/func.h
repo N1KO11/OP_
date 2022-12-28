7c5b722ca0d5bf2dcbe6de00a6fc740b1bee7da0
Никита <112944783+N1KO11@users.noreply.github.com>
2022-12-28 21:29:56 +0300

    Create func.h

---
+++ 11/func.h
@@ -0,0 +1,12 @@
+#ifndef LABA11_FUNC_H
+#define LABA11_FUNC_H
+struct Parall
+{
+    int x1, y1;
+    int x2, y2;
+    int x3, y3;
+    int x4, y4;
+};
+int S(struct Parall PC);
+float P(struct Parall PC);
+#endif
